42a9681fb896c034913d57783c5065a3cb5d0938
peplxx <seregaopkymelnikov@yandex.ru>
2024-11-13 23:00:27 +0300

    add: id's mixins

---
+++ backend/app/db/models/mixins/index.py
@@ -0,0 +1,24 @@
+from uuid import uuid4, UUID
+
+import sqlalchemy as sa
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class UUIDMixin:
+    __abstract__ = True
+    id = sa.Column(sa.UUID, nullable=False, primary_key=True, default=uuid4)
+
+    @classmethod
+    async def find_by_id(cls, session: AsyncSession, entity_id: UUID):
+        return await session.scalar(select(cls).where(cls.id == entity_id))
+
+class IDMixin:
+    __abstract__ = True
+    id = sa.Column(sa.Integer, nullable=False, primary_key=True, autoincrement=True)
+
+    @classmethod
+    async def find_by_id(cls, session: AsyncSession, entity_id: int):
+        return await session.scalar(select(cls).where(cls.id == entity_id))
+    
+__all__ = ["UUIDMixin", "IDMixin"]
