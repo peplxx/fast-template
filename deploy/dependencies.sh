4fbbe4cabd8933849fcd460d833189ff777ea199
peplxx <seregaopkymelnikov@yandex.ru>
2024-12-17 22:46:41 +0300

    fix: deps script

--- deploy/dependencies.sh
+++ deploy/dependencies.sh
@@ -1,23 +1,31 @@
 # Installs Dependencies: Make, Docker Engine and Docker compose Addon
 
 # 1: Make
-sudo apt-get update
-sudo apt install make
+if ! command -v make &> /dev/null; then
+    sudo apt install -y make
+fi
 
 # 2: Docker Engine
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+if ! command -v docker &> /dev/null; then
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install -y ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    # Install Docker Engine
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+fi
 
 # 3: Docker compose
-sudo apt-get install docker-compose-plugin
+if ! command -v docker compose &> /dev/null; then
+    sudo apt-get install -y docker-compose-plugin
+fi
