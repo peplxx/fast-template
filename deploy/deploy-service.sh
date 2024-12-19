de153234bb6d314a859e8d28d038cc69ef561521
peplxx <seregaopkymelnikov@yandex.ru>
2024-12-19 22:14:01 +0300

    fix: typos in deploy script

--- deploy/deploy-service.sh
+++ deploy/deploy-service.sh
@@ -1,7 +1,10 @@
 # Fill unit path with pwd
-sudo sed -i "s|^WorkingDirectory=.*|WorkingDirectory=$(pwd)|" /etc/systemd/system/fastapi-app.service
+sudo sed -i "s|^WorkingDirectory=.*|WorkingDirectory=$(pwd)|" deploy/fastapi-app.service
+sudo sed -i "s|^User=.*|User=$(whoami)|" deploy/fastapi-app.service
+
+sudo cp deploy/fastapi-app.service /etc/systemd/system/fastapi-app.service
 
 sudo systemctl daemon-reload
-sudo systemctl enable fastapi.service
-sudo systemctl start fastapi.service.service
-sudo systemctl status fastapi.service.service
+sudo systemctl enable fastapi-app.service
+sudo systemctl start fastapi-app.service
+sudo systemctl status fastapi-app.service
