5e1e2cf88b79c738e332a1dec671ba492b8e23e1
peplxx <seregaopkymelnikov@yandex.ru>
2024-12-20 22:18:01 +0300

    chore: move auth fixtures

--- backend/tests/fixtures/__init__.py
+++ backend/tests/fixtures/__init__.py
@@ -1,4 +1,3 @@
 from .database import postgres, async_engine, session
-from .user import test_user, user_data
 
-__all__ = ["postgres", "async_engine", "session", "test_user", "user_data"]
+__all__ = ["postgres", "async_engine", "session"]
